03ce40b3e9baa58f2f0974bc69f2d0029ba7cea3
yeison <yeison.roa.ortega@gmail.com>
2023-06-20 22:13:35 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,7 @@
+$('#menu-btn').click(function () {
+    $('nav .navigation ul').addClass('active')
+});
+
+$('#menu-close').click(function () {
+    $('nav .navigation ul').removeClass('active')
+});
